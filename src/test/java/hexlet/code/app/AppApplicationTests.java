2b767b44d44a6dbeb7f623751be036320b0b4210
adm134 <adm134@yandex.ru>
2022-01-24 11:00:51 +0300

    Установка gradle wrapper 7.2 и понижение версии JDK до 16

--- src/test/java/hexlet/code/app/AppApplicationTests.java
+++ src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,13 +1,28 @@
 package hexlet.code.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class AppApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
-	void contextLoads() {
-	}
+	void testUnauthenticatedRootPage() throws Exception {
+		MockHttpServletResponse response = mockMvc
+				.perform(get("/welcome"))
+				.andReturn()
+				.getResponse();
 
+		assertThat(response.getStatus()).isEqualTo(200);
+	}
 }
