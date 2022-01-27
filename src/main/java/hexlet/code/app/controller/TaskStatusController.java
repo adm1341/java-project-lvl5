f327fbafe5cee6fb6869b7564a2d6f539e747de2
adm134 <adm134@yandex.ru>
2022-01-27 12:01:14 +0300

    CRUD Статусов

---
+++ src/main/java/hexlet/code/app/controller/TaskStatusController.java
@@ -0,0 +1,72 @@
+package hexlet.code.app.controller;
+
+import hexlet.code.app.dto.TaskStatusDto;
+import hexlet.code.app.model.TaskStatus;
+import hexlet.code.app.repository.TaskStatusRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static hexlet.code.app.controller.TaskStatusController.TASK_STATUS_CONTROLLER_PATH;
+import static org.springframework.http.HttpStatus.CREATED;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("${base-url}" + TASK_STATUS_CONTROLLER_PATH)
+public class TaskStatusController {
+
+    public static final String TASK_STATUS_CONTROLLER_PATH = "/statuses";
+    public static final String ID = "/{id}";
+
+    private final TaskStatusRepository taskStatusRepository;
+
+
+    @Operation(summary = "Create new user")
+    @ApiResponse(responseCode = "201", description = "User created")
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public TaskStatus registerNew(@RequestBody @Valid final TaskStatusDto dto) {
+        TaskStatus taskStatus = new TaskStatus();
+        taskStatus.setName(dto.getName());
+        return taskStatusRepository.save(taskStatus);
+    }
+
+
+    @ApiResponses(@ApiResponse(responseCode = "200", content =
+    @Content(schema = @Schema(implementation = TaskStatus.class))
+    ))
+    @GetMapping
+    public List<TaskStatus> getAll() {
+        return taskStatusRepository.findAll()
+                .stream()
+                .toList();
+    }
+
+    @ApiResponses(@ApiResponse(responseCode = "200"))
+    @GetMapping(ID)
+    public TaskStatus getTaskStatusById(@PathVariable final Long id) {
+        return taskStatusRepository.findById(id).get();
+    }
+
+    @PutMapping(ID)
+    public TaskStatus update(@PathVariable final long id, @RequestBody @Valid final TaskStatusDto dto) {
+        final TaskStatus taskStatusToUpdate = taskStatusRepository.findById(id).get();
+        taskStatusToUpdate.setName(dto.getName());
+        return taskStatusRepository.save(taskStatusToUpdate);
+    }
+
+    @DeleteMapping(ID)
+    public void delete(@PathVariable final long id) {
+        taskStatusRepository.deleteById(id);
+    }
+
+
+}
